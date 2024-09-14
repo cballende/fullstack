0a72d89dcf3d53f2cd75bc4c6b97885854893022
cballende <cba.allende@gmail.com>
2024-09-14 12:40:39 -0300

    inner crud

--- 2_cuatrimestre/4_oop/Class_quiz_9_9/class.Registro.ts
+++ 2_cuatrimestre/4_oop/Class_quiz_9_9/class.Registro.ts
@@ -14,11 +14,16 @@ export class RegistroAutomotor {
     this.muebles=[];
   }
 
+  private getMuebleLgth():(number){
+    return this.muebles.length;
+  }
+  
+  
   private getMueble():(Vehiculo|undefined){
     return;
   }
-
-  private setMueble(){
+  
+  private setMueble(ix:number){
     
   }
   
@@ -26,13 +31,45 @@ export class RegistroAutomotor {
     this.muebles.push(obj);
   }
 
-  private updateMueble(){
-    
+  private clearMueble(idx:number):boolean{
+    if(idx>=0){
+      this.muebles = this.muebles.splice(idx,1);
+      return true;
+    }
+    return false;
   }
 
-  private clearMueble(){
+  private updateMueble(s:string,d:Vehiculo):boolean{
+
+    return isNaN (this.muebles.filter(miFunc)[0]);
+
+    function miFunc(value: Mueble): (boolean) {
+
+      if (value.patente.localeCompare(s) == 0){
+        if ( value.vehiculo.getChasis().localeCompare(d.getChasis()) != 0 )
+          value.vehiculo.setChasis(d.getChasis();
+        if ( value.vehiculo.getMarca().localeCompare(d.getMarca()) != 0 )
+          value.vehiculo.setMarca(d.getMarca();
+        if ( value.vehiculo.getModelo().localeCompare(d.getModelo()) != 0 )
+          value.vehiculo.setModelo(d.getModelo();
+        if ( value.vehiculo.getCilindrada().localeCompare(d.getCilindrada()) != 0 )
+          value.vehiculo.setCilindrada(d.getCilindrada();
+        return (true);
+      }
+      return (false);
+    }
   }
-  
+
+
+  private searchMueble(s:string):(Mueble){
+    return this.muebles.filter(miFunc)[0];
+    function miFunc(value: Mueble): (boolean) {
+      return (value.patente.localeCompare(s) == 0);
+    }
+  }
+
+
+  /*INTERFACE : public methods */
   public agregarVehiculo(vehiculo:Vehiculo):void{
     let obj:Mueble;
     let toTrunc:number;
@@ -46,19 +83,19 @@ export class RegistroAutomotor {
         this.putMueble(obj);
   }
 
-  public buscarVehiculo(patente:string):(Vehiculo|undefined){
-    return this.getMueble(patente); 
+  public buscarVehiculo(patente:string):(Mueble|undefined){
+    return this.searchMueble(patente);    
   }
 
-  public modificarVehiculo(patente:string,datosActualizados:Partial):void{
-    if (this.getMueble(patente)){
-      this.updateMueble(patente);
+  public modificarVehiculo(s:string,d:Vehiculo):void{
+    if (this.searchMueble(s)){
+      this.updateMueble(s,d);
     }
   }
 
-  public eliminarVehiculo(patente:string):void{
-    if (this.getMueble(patente)){
-      this.clearMueble(patente);
+  public eliminarVehiculo(s:string):void{
+    if (this.searchMueble(s)){
+      this.clearMueble(i);
     }
   }
 
--- 2_cuatrimestre/4_oop/Class_quiz_9_9/class.vehiculo.ts
+++ 2_cuatrimestre/4_oop/Class_quiz_9_9/class.vehiculo.ts
@@ -7,36 +7,46 @@ export class Vehiculo{
   protected chasis:string;
 
   constructor(o){
-            
     this.marca      = o.marca;
     this.modelo     = o.modelo;
     this.cilindrada = o.cilindrada;
     this.chasis     = o.chasis;
-
   }
 
   public getMarca():string{
-
     return this.marca;
-
   }
 
   public getModelo():string{
-
     return this.modelo;
-
-}
-
-  public getCilindrada():string{
-
-    return this.cilindrada;
-
   }
 
+  public getCilindrada():string{
+    return this.cilindrada;
+  }
   public getChasis():string{
-
     return this.chasis;
+  }
 
+  
+  public setMarca(s:string):boolean{
+    this.marca=s;
+    return this.marca.localeCompare(s) == 0;
+  }
+
+  public setModelo(s:string):boolean{
+    this.modelo=s;
+    return this.modelo.localeCompare(s) == 0;
+  }
+
+  public setCilindrada(s:string):boolean{
+    this.cilindrada=s;
+    return this.cilindrada.localeCompare(s) == 0;
+  }
+
+  public setChasis(s:string):boolean{
+    this.chasis=s;
+    return this.chasis.localeCompare(s) == 0;   
   }
       
   protected toStringB():string{
