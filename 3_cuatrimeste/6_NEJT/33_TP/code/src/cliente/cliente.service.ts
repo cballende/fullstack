7b7219afd83f1abbcd1e5061378bdd00d5919263
cballende <cba.allende@gmail.com>
2025-06-16 21:06:55 -0300

    33_tp

---
+++ 3_cuatrimeste/6_NEJT/33_TP/code/src/cliente/cliente.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateClienteDto } from './dto/create-cliente.dto';
+import { UpdateClienteDto } from './dto/update-cliente.dto';
+
+import { Cliente } from './entities/cliente.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ClienteService {
+
+  constructor(
+    @InjectRepository(Cliente)
+    private readonly clienteRepository: Repository<Cliente>,
+  ) {}
+
+  create(createClienteDto: CreateClienteDto) {
+    return 'This action adds a new cliente';
+  }
+
+  findAll() {
+    return `This action returns all cliente`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} cliente`;
+  }
+
+  update(id: number, updateClienteDto: UpdateClienteDto) {
+    return `This action updates a #${id} cliente`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} cliente`;
+  }
+}
---
+++ 3_cuatrimeste/6_NEJT/33_TP/code/static copy/main.js
@@ -0,0 +1,61 @@
+const API_URL = 'http://localhost:3000/cliente';
+
+let btnConsulta = document.getElementById("getId");
+btnConsulta.addEventListener("click",obtenerCliente);
+
+let btnEliminar = document.getElementById("deleteId");
+btnEliminar.addEventListener("click",eliminarCliente);
+
+let btnCrear = document.getElementById("createBtn");
+btnCrear.addEventListener("click",crearCliente);
+
+
+
+
+async function obtenerCliente(){
+    const id = document.getElementById("getIdinput").value;
+    const respuesta = await fetch(API_URL + "/" + id);
+    const data = await respuesta.json();
+    document.getElementById("getResult").textContent = JSON.stringify(data,null,2);
+}
+
+async function eliminarCliente(){
+    const id = document.getElementById("inputDeleteId").value;
+    try{
+        const respuesta = await fetch(API_URL + "/" + id,
+            {method:'DELETE'});
+            if(!respuesta.ok){
+                throw new Error("no se pudo eliminar al cliente");
+            }
+            const data = await respuesta.json();
+            document.getElementById("deleteResult").innerHTML = "Cliente eliminado";
+    }catch(error){
+        console.log(error.message + " no se pudo eliminar el cliente");
+    };
+}
+
+
+async function crearCliente(){
+    const cliente = {
+        nombre: document.getElementById("nombre").value,
+        apellido:document.getElementById("apellido").value,
+        direccion:document.getElementById("direccion").value,
+        activo:document.getElementById("activo").value
+    }
+
+    try{
+        const respuesta = await fetch(API_URL,
+            { method:'POST',
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify(cliente),
+        });
+        if(!respuesta.ok){
+            throw new Error("no se pudo crear el cliente");
+        }else{
+            document.getElementById("createResult").innerHTML = "Cliente Creado!!";
+        }
+        }
+        catch(error){
+             document.getElementById("createResult").innerHTML = "Cliente NO SE PUDO CREAR!! "+error.message;
+        }
+}
