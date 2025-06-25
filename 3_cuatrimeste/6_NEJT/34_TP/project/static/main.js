7abe25cab793f25efcf83669acaf78b3003db48f
cballende <cba.allende@gmail.com>
2025-06-24 22:31:02 -0300

    34_tp
    to test.

--- 3_cuatrimeste/6_NEJT/34_TP/project/static/main.js
+++ 3_cuatrimeste/6_NEJT/34_TP/project/static/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/cliente';
+const API_URL = 'http://localhost:3000/';
 
 let btnConsulta = document.getElementById("getId");
 btnConsulta.addEventListener("click",obtenerCliente);
@@ -17,15 +17,31 @@ let btnActualizar = document.getElementById("updateBtn");
 btnActualizar.addEventListener("click",actualizarCliente);
 
 
+
+let selFac = document.getElementById("select_update_factura");
+selFac.addEventListener("click",obtenerFacturas);
+selFac.addEventListener("change",actualizarDataFactura);
+
+let btnActFac = document.getElementById("updateFacturaBtn");
+btnActFac.addEventListener("click",actualizarFactura);
+
+let selPro = document.getElementById("select_update_producto");
+selPro.addEventListener("click",obtenerProductos);
+selPro.addEventListener("change",actualizarDataProducto);
+
+let btnActPro = document.getElementById("updateProductoBtn");
+btnActPro.addEventListener("click",actualizarProducto);
+
+
 async function obtenerCliente(){
     const id = document.getElementById("getIdinput").value;
-    const respuesta = await fetch(API_URL + "/" + id);
+    const respuesta = await fetch(API_URL + "cliente/" + id);
     const data = await respuesta.json();
     document.getElementById("getResult").textContent = JSON.stringify(data,null,2);
 }
 
 async function obtenerClientes(){
-    const respuesta = await fetch(API_URL);
+    const respuesta = await fetch(API_URL+"cliente");
     const data = await respuesta.json();
     let dSel = document.getElementById("select_update");
     let dOps =dSel.querySelectorAll("option");
@@ -45,7 +61,7 @@ async function obtenerClientes(){
 async function eliminarCliente(){
     const id = document.getElementById("inputDeleteId").value;
     try{
-        const respuesta = await fetch(API_URL + "/" + id,
+        const respuesta = await fetch(API_URL + "cliente/" + id,
             {method:'DELETE'});
             if(!respuesta.ok){
                 throw new Error("no se pudo eliminar al cliente");
@@ -67,7 +83,7 @@ async function crearCliente(){
     }
 
     try{
-        const respuesta = await fetch(API_URL,
+        const respuesta = await fetch(API_URL+"cliente",
             { method:'POST',
             headers: {'Content-Type': 'application/json'},
             body:JSON.stringify(cliente),
@@ -95,7 +111,7 @@ async function actualizarCliente(){
         }
     
         try{
-            const respuesta = await fetch(  API_URL+'/'+id,
+            const respuesta = await fetch(  API_URL+'cliente/'+id,
                                             {   method:'PUT',
                                                 headers: {'Content-Type': 'application/json'},
                                                 body:JSON.stringify(cliente)
@@ -132,6 +148,150 @@ function actualizarDataCliente(){
             document.getElementById("apellido_update").value= oData.apellido ;
             document.getElementById("direccion_update").value= oData.direccion ;
             document.getElementById("activo_update").value= oData.activo;
+        }
+    } 
+}
+
+
+
+
+async function obtenerFacturas(){
+    const respuesta = await fetch(API_URL+"factura");
+    const data = await respuesta.json();
+    let dSel = document.getElementById("select_update_factura");
+    let dOps =dSel.querySelectorAll("option");
+    if (dOps.length)
+        dOps.forEach(element => {
+            element.remove();
+        });
+    let dOpt;
+    data.forEach(element => {
+        dOpt= document.createElement('option');
+        dOpt.value=element.id;
+        dOpt.textContent=JSON.stringify(element,null,2);
+        dSel.appendChild(dOpt);
+    });    
+}
+
+
+async function actualizarFactura(){
+    let id= document.getElementById("id_update_factura").value;
+    if (id!=undefined) {
+        
+        const factura = {        
+            nombre: document.getElementById("nombre_update_factura").value,
+            apellido:document.getElementById("apellido_update_factura").value,
+            direccion:document.getElementById("direccion_update_factura").value,
+            activo:document.getElementById("activo_update_factura").value
+        }
+    
+        try{
+            const respuesta = await fetch(  API_URL+'factura/'+id,
+                                            {   method:'PUT',
+                                                headers: {'Content-Type': 'application/json'},
+                                                body:JSON.stringify(factura)
+                                            }
+                                        );
+                                        console.log(respuesta);
+            if(!respuesta.ok){
+                throw new Error("no se pudo modificar el Factura");
+            }else{
+                document.getElementById("updateFacturaResult").innerHTML = "Factura Modificada!!";
+            }
+        }catch(error){
+            document.getElementById("updateFacturaResult").innerHTML = "Factura NO SE PUDO MODIFICAR!! "+error.message;
+        }
+    }else
+        document.getElementById("updateFacturaResult").innerHTML = "Factura NO SE PUDO MODIFICAR!! "+error.message;
+
+}
+
+function actualizarDataFactura(){
+    let dSel = document.getElementById("select_update_factura");
+    let dOps=dSel.querySelectorAll("option");
+    if(dOps.length)
+        dOps.forEach(searchFun);
+    
+    function searchFun(elem) {
+        let oData= JSON.parse(elem.text);        
+        
+        //console.log(oData);
+        if (oData.id == dSel.value){
+            document.getElementById("id_update_factura").value= oData.id ;
+            document.getElementById("nombre_update_factura").value= oData.nombre ;
+            document.getElementById("apellido_update_factura").value= oData.apellido ;
+            document.getElementById("direccion_update_factura").value= oData.direccion ;
+            document.getElementById("activo_update_factura").value= oData.activo;
+        }
+    } 
+}
+
+async function obtenerProductos(){
+    const respuesta = await fetch(API_URL+"producto");
+    const data = await respuesta.json();
+    let dSel = document.getElementById("select_update_producto");
+    let dOps =dSel.querySelectorAll("option");
+    if (dOps.length)
+        dOps.forEach(element => {
+            element.remove();
+        });
+    let dOpt;
+    data.forEach(element => {
+        dOpt= document.createElement('option');
+        dOpt.value=element.id;
+        dOpt.textContent=JSON.stringify(element,null,2);
+        dSel.appendChild(dOpt);
+    });    
+}
+
+async function actualizarProducto(){
+    let id= document.getElementById("id_update_producto").value;
+    if (id!=undefined) {
+        
+        const producto = {        
+            nombre: document.getElementById("nombre_update_producto").value,
+            apellido:document.getElementById("apellido_update_producto").value,
+            direccion:document.getElementById("direccion_update_producto").value,
+            activo:document.getElementById("activo_update_producto").value
+        }
+    
+        try{
+            const respuesta = await fetch(  API_URL+'producto/'+id,
+                                            {   method:'PUT',
+                                                headers: {'Content-Type': 'application/json'},
+                                                body:JSON.stringify(producto)
+                                            }
+                                        );
+                                        console.log(respuesta);
+            if(!respuesta.ok){
+                throw new Error("no se pudo modificar el Producto");
+            }else{
+                document.getElementById("updateProductoResult").innerHTML = "Producto Modificado!!";
+            }
+        }catch(error){
+            document.getElementById("updateProductoResult").innerHTML = "Producto NO SE PUDO MODIFICAR!! "+error.message;
+        }
+    }else
+        document.getElementById("updateProductoResult").innerHTML = "Producto NO SE PUDO MODIFICAR!! "+error.message;
+
+}
+
+function actualizarDataProducto(){
+    let dSel = document.getElementById("select_update_producto");
+    let dOps=dSel.querySelectorAll("option");
+    if(dOps.length)
+        dOps.forEach(searchFun);
+    
+    function searchFun(elem) {
+        let oData= JSON.parse(elem.text);        
+        
+        //console.log(oData);
+        if (oData.id == dSel.value){
+            document.getElementById("id_update_producto").value= oData.id ;
+            document.getElementById("nombre_update_producto").value= oData.nombre ;
+            document.getElementById("apellido_update_producto").value= oData.apellido ;
+            document.getElementById("direccion_update_producto").value= oData.direccion ;
+            document.getElementById("activo_update_producto").value= oData.activo;
         }
     } 
 }
